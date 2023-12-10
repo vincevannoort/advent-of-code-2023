3cb6f102389da0e2eaeb9702aed7995bd7ee42be
Vince van Noort <vincevannoort@gmail.com>
2023-12-10 15:13:09 +0100

    feat(aoc): solve day 9 (part 2)

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,11 +1,20 @@
 use itertools::Itertools;
 
-fn find_differences(numbers: Vec<i64>, last_numbers: Vec<i64>) -> (Vec<i64>, Vec<i64>) {
+enum Part {
+    One,
+    Two,
+}
+
+fn find_differences(part: Part, numbers: Vec<i64>, last_numbers: Vec<i64>) -> (Vec<i64>, Vec<i64>) {
     // calculate differences
     let differences = numbers
         .iter()
         .tuple_windows()
-        .map(|(a, b)| b - a)
+        .map(|(a, b)| match part {
+            // there must be a better way, but my brain is not smart enough
+            Part::One => b - a,
+            Part::Two => a - b,
+        })
         .collect_vec();
 
     // stop when the differences are all 0
@@ -17,23 +26,20 @@ fn find_differences(numbers: Vec<i64>, last_numbers: Vec<i64>) -> (Vec<i64>, Vec
     let mut last_numbers = last_numbers;
     last_numbers.push(*differences.last().unwrap());
 
-    find_differences(differences, last_numbers)
+    find_differences(part, differences, last_numbers)
 }
 
 pub fn part_one(input: &str) -> Option<i64> {
     let answer: i64 = input
         .lines()
         .map(|line| {
-            dbg!(line);
             let numbers = line
                 .split_whitespace()
                 .map(|number| number.parse::<i64>().unwrap())
                 .collect_vec();
 
             let last_number = *numbers.last().unwrap();
-            let (_, last_numbers) = find_differences(numbers, vec![last_number]);
-
-            dbg!(&last_numbers);
+            let (_, last_numbers) = find_differences(Part::One, numbers, vec![last_number]);
             last_numbers.iter().sum::<i64>()
         })
         .sum();
@@ -41,7 +47,29 @@ pub fn part_one(input: &str) -> Option<i64> {
 }
 
 pub fn part_two(input: &str) -> Option<i64> {
-    None
+    let answer: i64 = input
+        .lines()
+        .map(|line| {
+            let numbers = line
+                .split_whitespace()
+                .map(|number| number.parse::<i64>().unwrap())
+                .rev()
+                .collect_vec();
+
+            let last_number = *numbers.last().unwrap();
+            let (_, last_numbers) = find_differences(Part::Two, numbers, vec![last_number]);
+            let mut last_numbers = last_numbers;
+            last_numbers.push(0);
+
+            last_numbers
+                .into_iter()
+                .rev()
+                .reduce(|acc, e| e - acc)
+                .unwrap()
+        })
+        .sum();
+
+    Some(answer)
 }
 
 advent_of_code::main!(9);
@@ -59,6 +87,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", 9));
-        assert_eq!(result, Some(5));
+        assert_eq!(result, Some(2));
     }
 }
